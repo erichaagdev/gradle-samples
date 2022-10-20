c242e1a633bfc81477c9b46cc3d100de009ed61c
Eric Haag <eah0592@gmail.com>
2022-10-20 12:21:18 -0500

    Add sample build for downloading an OpenAPI specification and generating code

---
+++ remote-openapi-spec/build.gradle.kts
@@ -0,0 +1,24 @@
+import de.undercouch.gradle.tasks.download.Download
+
+plugins {
+  base
+  id("de.undercouch.download") version "5.3.0"
+  id("org.openapi.generator") version "6.2.0"
+}
+
+val openApiDownloadDestination = layout.buildDirectory.file("openapi/spec/petstore.yaml")
+val openApiDownload by tasks.registering(Download::class) {
+  src("https://raw.githubusercontent.com/OAI/OpenAPI-Specification/3.0.3/examples/v3.0/petstore.yaml")
+  dest(openApiDownloadDestination)
+  onlyIfModified(true)
+}
+
+openApiGenerate {
+  generatorName.set("java")
+  inputSpec.set(openApiDownloadDestination.map { it.toString() })
+  outputDir.set(layout.buildDirectory.dir("generated/openapi").map { it.toString() })
+}
+
+tasks.named("openApiGenerate") {
+  dependsOn(openApiDownload)
+}
